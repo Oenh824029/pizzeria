c6014caa799596a8a460930aebbea10fa8c25a48
oscar naranjo <oscarelmono123@gmail.com>
2025-04-06 11:50:45 -0500

    vista de pizza

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+
+use App\Http\Controllers\PizzaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// rutas de la vista pizza
+
+Route::get('/pizzas',[PizzaController::class, 'index']);
