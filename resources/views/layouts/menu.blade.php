e464e4108f8c297e05d5557718b231d5d0fd3f14
oscar naranjo <oscarelmono123@gmail.com>
2025-04-14 19:19:15 -0500

    se agrega login y cerrar sesión en menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -4,52 +4,73 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    
+
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('pizzas.index') }}">Pizzas</a>
+
+        <!-- Menú de Productos -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="productosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Productos
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="productosDropdown">
+            <li><a class="dropdown-item" href="{{ route('pizzas.index') }}">Pizzas</a></li>
+            <li><a class="dropdown-item" href="{{ route('pizza_sizes.index') }}">Tamaño Pizzas</a></li>
+            <li><a class="dropdown-item" href="{{ route('pizza_ingredients.index') }}">Ingredientes Pizza</a></li>
+            <li><a class="dropdown-item" href="{{ route('ingredients.index') }}">Ingredientes</a></li>
+            <li><a class="dropdown-item" href="{{ route('extra_ingredients.index') }}">Ingredientes Extra</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('ingredients.index') }}">Ingredientes</a>
+
+        <!-- Menú de Operaciones -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="operacionesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Operaciones
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="operacionesDropdown">
+            <li><a class="dropdown-item" href="{{ route('orders.index') }}">Ordenes</a></li>
+            <li><a class="dropdown-item" href="{{ route('order_pizzas.index') }}">Ordenes de las Pizzas</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('extra_ingredients.index') }}">Ingredientes Extra</a>
+
+        <!-- Menú de Administración -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="administracionDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Administración
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="administracionDropdown">
+            <li><a class="dropdown-item" href="{{ route('branches.index') }}">Sucursales</a></li>
+            <li><a class="dropdown-item" href="{{ route('suppliers.index') }}">Proveedores</a></li>
+            <li><a class="dropdown-item" href="{{ route('raw_materials.index') }}">Materias Primas</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('branches.index') }}">Sucursales</a>
+
+        <!-- Menú de Personas -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="personasDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Personas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="personasDropdown">
+            <li><a class="dropdown-item" href="{{ route('users.index') }}">Usuarios</a></li>
+            <li><a class="dropdown-item" href="{{ route('clients.index') }}">Clientes</a></li>
+            <li><a class="dropdown-item" href="{{ route('employees.index') }}">Empleados</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('suppliers.index') }}">Proveedores</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('raw_materials.index') }}">Materias Primas</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('users.index') }}">Usuarios</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('clients.index') }}">Clientes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('employees.index') }}">Empleados</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('pizza_sizes.index') }}">Tamaño Pizzas</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('pizza_ingredients.index') }}">Ingredientes Pizza</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('orders.index') }}">Ordenes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('order_pizzas.index') }}">Ordenes de las Pizzas</a>
-        </li>
+
       </ul>
 
       <!-- Botón de Cerrar Sesión -->
-   
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit" class="nav-link btn btn-link text-white text-decoration-none" style="padding: 0; border: none; background: none;">
+              Cerrar sesión
+            </button>
+          </form>
+        </li>
+      </ul>
+
     </div>
   </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use App\Http\Controllers\PizzaController;
 use App\Http\Controllers\IngredientsController;
 use App\Http\Controllers\ExtraIngredienteController;
@@ -15,14 +14,23 @@ use App\Http\Controllers\PizzaIngredientController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderPizzaController;
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// rutas de la vista pizza
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // rutas de la vista pizza
 
 Route::get('/pizzas',[PizzaController::class, 'index'])->name('pizzas.index');
 Route::post('/pizzas',[PizzaController::class,'store'])->name('pizzas.store');
@@ -123,6 +131,6 @@ Route::get('/orders/{order}/edit',[OrderController::class, 'edit'])->name('order
 Route::get('/order_pizzas',[OrderPizzaController::class, 'index'])->name('order_pizzas.index');
 Route::post('/order_pizzas',[OrderPizzaController::class,'store'])->name('order_pizzas.store');
 Route::get('/order_pizzas/create',[OrderPizzaController::class, 'create'])->name('order_pizzas.create');
+});
 
-
-
+require __DIR__.'/auth.php';
